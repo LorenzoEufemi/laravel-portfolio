f1585702f8a32a9b660164c8b754f439c27e4473
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-04-10 10:48:48 +0200

    Add API endpoints for project management with index and show functionality

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\ProjectController;
+
+Route::get('projects', [ProjectController::class, 'index']);
+
+Route::get('projects/{project}', [ProjectController::class, 'show']);
